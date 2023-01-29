e66ae2ed13b09b4dd718b4ff3190c01f79f59e03
Luis Chica Moncayo <luischicamoncayo@hotmail.com>
2023-01-29 17:06:58 -0500

    credential

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { OrganizationModule } from './organization/organization.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type:'postgres',
-//      url: 'postgresql://luis:zvY699YtkDpWmQL57jd6-Q@rigid-croaker-8394.7tt.cockroachlabs.cloud:26257/repositories?sslmode=verify-full',
+//      url: process.env.DB_URL,
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
